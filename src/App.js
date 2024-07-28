4855e311e75a430e112ff0fa6cb6833562c72549
daizhilian <daizhilian@gf.com.cn>
2024-07-28 18:24:35 +0800

    feat: mol demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { StandaloneStructService } from 'ketcher-standalone'
+import { Ketcher } from 'ketcher-core';
+import { useEffect, useRef, useState } from 'react';
+
+const structServiceProvider = new StandaloneStructService()
+const ketcher = new Ketcher({}, structServiceProvider, {});
 
 function App() {
+  const [imgLink, setImgLink] = useState('');
+
+  useEffect(() => {
+    ketcher.generateImage('Oc1ccccc1', {
+      outputFormat: 'svg',
+      backgroundColor: "255, 255, 255"
+    }).then((res) => {
+      const url = window.URL.createObjectURL(res);
+      setImgLink(url)
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={imgLink} alt='SMILES'/>
     </div>
   );
 }
